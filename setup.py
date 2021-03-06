e7cf8f3ac371582e25c2b8938a676d46f01762f6
Mohammad Mahmoudi <7311957+mohammad2928@users.noreply.github.com>
2021-03-06 20:56:54 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='SLTev',
-    version='1.1.4',
+    version='1.1.5',
     author="Mohammad Mahmoudi",
     author_email="zaribar2928@gmail.com",
     description="a tool for evaluation",
