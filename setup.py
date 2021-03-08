ccdccb72635a6be1071eaaddaa6b3b704eedb09d
Dominik Macháček <Gldkslfmsd@users.noreply.github.com>
2021-03-08 15:01:19 +0100

    switching numpy version by python version (#50)
    * switching numpy version by python version
    
    * Require numpy<=1.19.5 for python<=3.6
    
    Co-authored-by: Matúš Žilinec <zilinec.m@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
+import sys
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+if sys.version_info[:2] >= (3,7):
+    numpy_version = "numpy"
+else:
+    # Later numpy versions are incompatible with Python <= 3.6
+    numpy_version = "numpy<=1.19.5"
 
 setup(
     name='SLTev',
@@ -13,7 +20,7 @@ setup(
     packages=['SLTev',],
     data_files = [ ('SLTev', ['SLTev/mwerSegmenter']) ],
     include_package_data=True,
-    install_requires=['numpy==1.19.5', 'gitpython', 'sacremoses', 'sacrebleu', 'gitdir', 'jiwer', 'filelock'],
+    install_requires=[numpy_version, 'gitpython', 'sacremoses', 'sacrebleu', 'gitdir', 'jiwer', 'filelock'],
     url="https://github.com/ELITR/SLTev.git",
     classifiers=[
         "Programming Language :: Python :: 3.6",
