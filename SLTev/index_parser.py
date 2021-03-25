958f758f0c87cca283fde1d6ac2d204b137a12c3
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-03-25 17:52:00 +0100

    Update README + more flexible parsing

--- SLTev/index_parser.py
+++ SLTev/index_parser.py
@@ -2,47 +2,65 @@ import os
 import sys
 import glob
 import re
+import json
 
 """
-Read an index file with meta-annotations and return tuples of (SOURCE, REFERENCE) absolute paths to file.
-Annotations specify the source and target file prefixes and apply to a specified directory.
-An index file can contain multiple annotations.
-Index file format:
+Read an index file with meta-annotations (SRC, REF, ALIGNMENT...)
 
-# SRC *.<PREFIX>
-# TRG *.<PREFIX>
+Meta-annotation format:
+# NAME -> *.<EXTENSION>
+
+Return an iterable of dicts containing paths to the specified files
+If invoked on the command line, return a JSON of the list of dicts
+Multiple directories can share the same meta-annotations, as long as there isn't a blank line between them
+Blank line resets the meta-annotations 
+SRC and REF are mandatory annotations
+
+Example: 
+
+# SRC -> *.<EXTENSION>
+# REF -> *.<EXTENSION>
 PATH_TO_DIRECTORY
+PATH_TO_ANOTHER_DIRECTORY_WITH_SAME_PREFIXES
+
+# SRC -> *.<EXTENSION>
+# REF -> *.<EXTENSION>
+PATH_TO_DIRECTORY_WITH_DIFFERENT_PREFIXES
 """
+
 
 def parseIndexFile(indexFilePath, testsetPath):
-    SRC = None
-    REF = None
+    fileExtensions = {} # Dict of file extensions
     with open(indexFilePath) as indexFile:
         for line in indexFile:
             line = line.rstrip()
-            # Line format:  # SRC: *.en.OSt
             if line.startswith("#"):
-                if line.startswith("# SRC "):
-                    SRC = line.split(" ")[2]
-                elif line.startswith("# REF "):
-                    REF = line.split(" ")[2]
-            elif len(line) > 0:
-                if not SRC or not REF:
-                    raise Exception("SRC or REF file suffix not specified")
-                sources = glob.glob(f"{testsetPath}/{line}/{SRC}")
-                references = glob.glob(f"{testsetPath}/{line}/{REF}")
+                if "->" in line:
+                    _, fileType, _, extension = line.split(" ")
+                    if not extension.startswith("*"):
+                        raise Exception(f"{line} -- extension must start with a *")
+                    fileExtensions[fileType] = extension 
+            elif len(line) == 0:
+                fileExtensions = {} 
+            else:
+                if "SRC" not in fileExtensions or "REF" not in fileExtensions:
+                    raise Exception(f"{line} -- SRC or REF not specified") 
+                sourceExtension = fileExtensions["SRC"]
+                sources = glob.glob(f"{testsetPath}/{line}/{sourceExtension}")
 
-                sourceSuffix = SRC[1:]
-                referenceSuffix = REF[1:]
+                # Source file is guaranteed to exist, verify all other requested files exist
                 for source in sources:
-                   reference = next(filter(lambda ref: re.sub(referenceSuffix + "$", sourceSuffix, ref) == source, references))
-                   if reference:
-                       yield (os.path.realpath(source), os.path.realpath(reference))
+                    evalEntry = {}
+                    for name, extension in fileExtensions.items():
+                        matchingFileName = re.sub(sourceExtension[1:] + "$", "", source) + extension[1:]
+                        if not os.path.exists(matchingFileName):
+                            raise Exception(f"{name} {extension} -- {matchingFileName} does not exist")
+                        evalEntry[name] = os.path.realpath(matchingFileName)
+                    yield (evalEntry)
 
 def main():
-    pairs = parseIndexFile(sys.argv[1], sys.argv[2])
-    for pair in pairs:
-        print(f"{pair[0]}\t{pair[1]}")
+    paths = [path for path in parseIndexFile(sys.argv[1], sys.argv[2])]
+    print(json.dumps(paths))
 
 if __name__ == "__main__":
     main()
